9fd978806943717dd2034f4d874a089b647079ff
matrixmandeep <mandeep@devs.matrixmarketers.com>
2023-11-06 17:37:32 +0530

    initail commit

---
+++ react-2k23/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from '@/scenes/navbar';
+
+function App() {
+
+  return (
+   <div className='app bg-gray-20'>App
+   <Navbar />
+   </div>
+  )
+}
+
+export default App
